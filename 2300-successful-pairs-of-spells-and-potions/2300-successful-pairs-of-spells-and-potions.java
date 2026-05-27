20e22768bda402559efefebef8a03414b3d43025
Nikolai Khriapov <nikolai.khriapov@nuaa.edu.cn>
2026-05-27 21:55:53 +0700

    Time: 52 ms (32.48%), Space: 146.8 MB (54.36%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        if (spells == null || spells.length == 0 || potions == null || potions.length == 0 || success < 1) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        Arrays.sort(potions);
+
+        int[] pairs = new int[spells.length];
+        for (int i = 0; i < pairs.length; i++) {
+            pairs[i] = countPotions(spells[i], potions, success);
+        }
+        return pairs;
+    }
+
+    private int countPotions(int spell, int[] potions, long success) {
+        int left = 0;
+        int right = potions.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long product = (long) spell * potions[mid];
+            if (product >= success) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return potions.length - left;
+    }
+}
+
+// time  - O(n log m)
+// space - O(n)
